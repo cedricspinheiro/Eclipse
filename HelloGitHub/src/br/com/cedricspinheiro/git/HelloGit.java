473ec88205d4e1b57fe9dcbe747122f9a4ff368d
Cedric Santos Pinheiro <119817010+cedricspinheiro@users.noreply.github.com>
2022-12-10 10:50:39 -0300

    Adicionei arquivos via upload

---
+++ HelloGitHub/src/br/com/cedricspinheiro/git/HelloGit.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.cedricspinheiro.git;
+
+/**
+ * @author nkkara
+ * Exemplo de integracao do eclipse com GitHub
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.printin("Hello Git"); 
+		
+	}
+
+}
---
+++ HelloGitHub/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author nkkara
+ *
+ */
+module HelloGitHub {
+}
